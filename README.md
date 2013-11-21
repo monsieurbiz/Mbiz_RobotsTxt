fd0a5dfc33e8878fa6c558dce6bc8d13d7304669
Léo Peltier <contact@leo-peltier.fr>
2013-11-21 15:17:09 +0100

    Working v1 of the module.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Mbiz_RobotsTxt
 This Magento plugin adds a way to edit the robots.txt file from the admin
 panel.
+The new option is located in `General > Web > Search Engines Optimization`.
---
+++ code/Model/RobotsTxt.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of Mbiz_RobotsTxt for Magento.
+ *
+ * @license All rights reserved.
+ * @author Léo Peltier <l.peltier@monsieurbiz.com>
+ * @category Mbiz
+ * @package Mbiz_RobotsTxt
+ * @copyright Copyright (c) 2013 Monsieur Biz (http://monsieurbiz.com/)
+ */
+class Mbiz_RobotsTxt_Model_RobotsTxt extends Mage_Core_Model_Config_Data
+{
+    /**
+     * @return string robots.txt file path.
+     */
+    protected function getRobotsTxtPath()
+    {
+        return Mage::getBaseDir('base') . '/robots.txt';
+    }
+
+
+    /// Rewrite robots.txt when the configuration is saved.
+    protected function _beforeSave()
+    {
+        parent::_beforeSave();
+        $contents = $this->getValue();
+        $ret = file_put_contents($this->getRobotsTxtPath(), $contents);
+
+        if ($ret === false)
+            throw new Exception('Failed to write robots.txt.');
+
+        return $this;
+    }
+
+
+    /**
+     * Load the value directly from the robots.txt file.
+     *
+     * If the file does not exist, discard what is in the magento config and
+     * load an empty string.
+     */
+    protected function _afterLoad()
+    {
+        parent::_afterLoad();
+        $path = $this->getRobotsTxtPath();
+
+        if(file_exists($path))
+            $this->setValue(file_get_contents($path));
+        else
+            $this->setValue('');
+    }
+}
